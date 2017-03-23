0f65cb45c72875e04589a8c378a02099c9df3a6d
Alex Reiff <alexander.reiff@mail.mcgill.ca>
2017-03-23 02:03:44 -0400

    Added server command to remove expired events from database

--- fb_mcbot/management/commands/expire_events.py
+++ fb_mcbot/management/commands/expire_events.py
@@ -1,9 +1,16 @@
 from django.core.management.base import BaseCommand
+from bot_mcgill.models import McgillEvent
+import json
+import _datetime
+import pytz
+from bot_services.user_service import UserService, Question
+from datetime import datetime
+from fb_mcbot.models import Event
 
 class Command(BaseCommand):
 
     help = 'Expires event objects which are out-of-date'
 
     def handle(self, *args, **options):
-        #print(Event.objects.filter(date__lt=datetime.datetime.now()).delete())
-        print('Hello World')
+        print(Event.objects.filter(event_time__lt=datetime.now()).delete())
+        #print('Hello World')
